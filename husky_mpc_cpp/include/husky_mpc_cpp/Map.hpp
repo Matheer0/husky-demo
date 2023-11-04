99db4e5b89aefa0ae8cfbeebdc1c4ffde747dc97
alex <alexqixuan@gmail.com>
2023-11-04 17:12:44 -0400

    new map head file

--- husky_mpc_cpp/include/husky_mpc_cpp/Map.hpp
+++ husky_mpc_cpp/include/husky_mpc_cpp/Map.hpp
@@ -17,20 +17,27 @@ class Map
 {
 public:
     Map () = default;
-    Map(double x_init, double y_init, double x_target, double y_target, double theta_target, double y_center, double y_offset);
-    void generate_circular_obstacles();
+    Map(double x_init, double y_init, double x_target, double y_target, double theta_target, double x_center, double y_center, double offset);
+    
     double x_init_;
     double y_init_;
     double x_target_;
     double y_target_;
     double theta_target_;
 
+    double x_center_;
     double y_center_;
-    double y_offset_;
+    double offset_;
+
+    double x_upper_limit_;
+    double x_lower_limit_;
     double y_upper_limit_;
     double y_lower_limit_;
 
     std::vector<Obstacle> obstacle_list;
+    std::vector<Obstacle> dummy_obstacles;
+
+    void generate_circular_obstacles();
 };
 
 #endif // MAP_HPP
